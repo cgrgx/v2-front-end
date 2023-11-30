c4ac1cda789b6e9eb3baf6f41746beb8e2339b1d
chiran gurung <grgchiran08@yahoo.com>
2023-11-29 19:09:04 -0500

    signup and signin added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
 import Landing from "./components/landing/Landing";
+import SignUp from "./components/auth/signup";
+import SignIn from "./components/auth/signin";
+import { Welcome } from "./components/app/welcome";
+
+//graphql end point
+const client = new ApolloClient({
+  uri: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+  cache: new InMemoryCache(),
+});
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  const [loadingUserData, setLoadingUserData] = useState(true);
+
+  // Access backend URL from environment variable
+  const backendUrl =
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+
+  useEffect(() => {
+    // Fetch user data when the app loads
+    console.log("Backend URL:", backendUrl);
+
+    // Construct the fetching URL and log it
+    const fetchUrl = `${backendUrl}/api/user`;
+    console.log("Fetching URL:", fetchUrl);
+
+    // Fetch user data when the app loads
+    fetch(fetchUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUserData(data.data);
+        setLoadingUserData(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error.message);
+        setLoadingUserData(false);
+      });
+  }, [backendUrl]); // Adding backendUrl as a dependency for useEffect
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path="/" element={<Landing backendUrl={backendUrl} />} />
+          <Route path="/signup" element={<SignUp backendUrl={backendUrl} />} />
+          <Route path="/signin" element={<SignIn backendUrl={backendUrl} />} />
+          <Route
+            path="/welcome"
+            element={
+              loadingUserData ? (
+                <p>Loading user data...</p>
+              ) : (
+                <Welcome userData={userData} />
+              )
+            }
+          />
+        </Routes>
+      </ApolloProvider>
     </Router>
   );
 }
--- src/components/landing/ui/header.js
+++ src/components/landing/ui/header.js
@@ -5,7 +5,7 @@ import logo from "../../../assets/trans_logo.png";
 
 function Header() {
   return (
-    <header className="absolute w-full z-30">
+    <header className="absolute w-full z-30 bg-gray-800">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-20">
           {/* Site branding */}
